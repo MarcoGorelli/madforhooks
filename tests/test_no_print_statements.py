473916a369523900dd8b1c9b84e261672c6d7043
Marco Gorelli <marcogorelli@protonmail.com>
2021-07-24 14:30:11 +0100

    no-print-statements

---
+++ tests/test_no_print_statements.py
@@ -0,0 +1,32 @@
+"""Test conda-env-sorter."""
+
+from pathlib import Path
+
+from _pytest.capture import capsys
+from _pytest.tmpdir import tmpdir
+
+from madforhooks.no_print_statements import main, no_print_statements
+
+ROOT = Path(__file__).parent.parent.parent
+
+
+def test_main(capsys: capsys) -> None:
+    src = "print(foo)"
+    no_print_statements(src, "t.py")
+    out, err = capsys.readouterr()
+    assert out == "t.py:1:0 found print statement\n"
+
+
+def test_main_noop(capsys: capsys) -> None:
+    src = ""
+    no_print_statements(src, "t.py")
+    out, err = capsys.readouterr()
+    assert out == ""
+
+
+def test_e2e(tmpdir: tmpdir, capsys: capsys) -> None:
+    with open(tmpdir / "t.py", "w") as fd:
+        fd.write("print(foo)\n")
+    main((str(tmpdir / "t.py"),))
+    out, err = capsys.readouterr()
+    assert "t.py:1:0 found print statement\n" in out
