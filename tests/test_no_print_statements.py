ada87f9d81e50aeb2e603679e0ac312282ebb3d6
MarcoGorelli <>
2022-12-24 12:14:15 +0000

    fixup again

--- tests/test_no_print_statements.py
+++ tests/test_no_print_statements.py
@@ -2,36 +2,35 @@
 
 from pathlib import Path
 
-from _pytest.capture import capsys
-from _pytest.tmpdir import tmpdir
+from typing import Any
 
 from madforhooks.no_print_statements import main, no_print_statements
 
 ROOT = Path(__file__).parent.parent.parent
 
 
-def test_main(capsys: capsys) -> None:
+def test_main(capsys: Any) -> None:
     src = "print(foo)"
     no_print_statements(src, "t.py")
     out, err = capsys.readouterr()
     assert out == "t.py:1:0 found print statement\n"
 
 
-def test_main_noop(capsys: capsys) -> None:
+def test_main_noop(capsys: Any) -> None:
     src = ""
     no_print_statements(src, "t.py")
     out, err = capsys.readouterr()
     assert out == ""
 
 
-def test_main_print_with_file(capsys: capsys) -> None:
+def test_main_print_with_file(capsys: Any) -> None:
     src = "print(foo, file=bar)"
     no_print_statements(src, "t.py")
     out, err = capsys.readouterr()
     assert out == ""
 
 
-def test_e2e(tmpdir: tmpdir, capsys: capsys) -> None:
+def test_e2e(tmpdir: Any, capsys: Any) -> None:
     with open(tmpdir / "t.py", "w") as fd:
         fd.write("print(foo)\n")
     main((str(tmpdir / "t.py"),))
